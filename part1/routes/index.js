cdaf51c980095ea2888d6cf800969c025ab6e945
ahhkhoa56 <anhkhoa.wm@gmail.com>
2025-06-20 02:49:04 +0000

    [automatic commit] 2025-06-20 02:49:03 UTC 203.221.169.149

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -65,8 +65,8 @@ router.get('/api/walkers/summary', async function(req, res) {
       FROM WalkRatings ra
       JOIN Users u
         ON ra.walker_id = u.user_id
-      GROUP BY (ra.walker_id, u.username)
-      WHERE u.role = 'walker'`
+      WHERE u.role = 'walker'
+      GROUP BY (ra.walker_id, u.username)`
     );
     return res.status(200).json(rows);
   } catch(err) {
