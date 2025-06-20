a813488cf31268bd5b272fe3f3cd06d21ac3a481
ahhkhoa56 <anhkhoa.wm@gmail.com>
2025-06-20 02:39:27 +0000

    [automatic commit] 2025-06-20 02:39:27 UTC 203.221.169.149

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -51,7 +51,7 @@ router.get('/api/walkrequests/open', async function(req, res) {
 router.get('/api/walkers/summary', async function(req, res) {
   try {
     const [rows] = await db.query(
-      `SELECT u.username AS walker_username
+      `SELECT u.username AS walker_username, COUNT()
       FROM WalkRequests r
       JOIN Dogs d
         ON r.dog_id = d.dog_id
