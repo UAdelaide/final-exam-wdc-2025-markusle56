21c482cdff1229dd93da9a0c024e9d40e504ef99
ahhkhoa56 <anhkhoa.wm@gmail.com>
2025-06-20 01:44:57 +0000

    [automatic commit] 2025-06-20 01:44:57 UTC 203.221.169.149

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -61,5 +61,6 @@ CREATE TABLE WalkRatings (
 INSERT INTO Users (username, email, password_hash, role) 
 VALUES
 ('alice123', 'alice@example.com', 'hashed123','owner'),
-('bobwalker', 'bob@example.com', 'hashed123','owner'),
-('carol123', 'carol@example.com', 'hashed123','owner')
+('bobwalker', 'bob@example.com', 'hashed456','walker'),
+('carol123', 'carol@example.com', 'hashed789','owner');
+
