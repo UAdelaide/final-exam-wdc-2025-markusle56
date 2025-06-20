d92c83dcbfa5064c371dc4975d3db6d839835094
ahhkhoa56 <anhkhoa.wm@gmail.com>
2025-06-20 02:29:35 +0000

    [automatic commit] 2025-06-20 02:29:35 UTC 203.221.169.149

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
 
 router.get('/api/dogs', async function(req, res) {
   try {
-    const [rows] = await db
+    const [rows] = await db.qu
   } catch(err) {
     console.error("Error geting dogs data")
   }
