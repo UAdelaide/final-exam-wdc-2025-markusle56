d2c3be21ba64a0c1cc78ac9877396e4a3a313211
ahhkhoa56 <anhkhoa.wm@gmail.com>
2025-06-20 02:35:22 +0000

    [automatic commit] 2025-06-20 02:35:22 UTC 203.221.169.149

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -24,4 +24,22 @@ router.get('/api/dogs', async function(req, res) {
     return res.sendStatus(500);
   }
 });
+
+router.get('/api/dogs', async function(req, res) {
+  try {
+    const [rows] = await db.query(
+      `SELECT
+        d.name AS dog_name,
+        d.size,
+        u.username AS owner_username
+      FROM Dogs d
+      JOIN Users u
+        ON d.owner_id = u.user_id`
+    );
+    return res.status(200).json(rows);
+  } catch(err) {
+    console.error("Error geting dogs data");
+    return res.sendStatus(500);
+  }
+});
 module.exports = router;
