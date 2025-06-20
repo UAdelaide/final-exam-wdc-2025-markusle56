b923f53f33a813354529cb3e694f55880e1b5e20
ahhkhoa56 <anhkhoa.wm@gmail.com>
2025-06-20 04:00:36 +0000

    [automatic commit] 2025-06-20 04:00:36 UTC 203.221.169.149

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -61,6 +61,7 @@ router.get('/api/walkers/summary', async function(req, res) {
           COUNT(*)
           FROM WalkApplications a
           JOIN WalkRequests re
+           ON a.request_id = re.request_id
           WHERE ra.request_id = re.request_id
             AND re.status = 'completed'
         ) AS completed_walks
