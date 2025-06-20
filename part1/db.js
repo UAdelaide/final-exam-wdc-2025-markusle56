08549272605e5e2495ee249f9bc7342b13efe4c2
ahhkhoa56 <anhkhoa.wm@gmail.com>
2025-06-20 03:00:02 +0000

    [automatic commit] 2025-06-20 03:00:02 UTC 203.221.169.149

---
+++ part1/db.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+    // host: 'localhost',
+    socketPath: '/var/run/mysqld/mysqld.sock',
+    user: 'root',
+    password: 'root',
+    database: 'DogWalkService'
+});
+
+module.exports = pool;
