0bb37e946356b837cde23dd44c8bf678b2d9972e
ahhkhoa56 <anhkhoa.wm@gmail.com>
2025-06-20 02:49:24 +0000

    [automatic commit] 2025-06-20 02:49:24 UTC 203.221.169.149

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -70,7 +70,7 @@ router.get('/api/walkers/summary', async function(req, res) {
     );
     return res.status(200).json(rows);
   } catch(err) {
-    console.error("Error geting request walk data");
+    console.error("Error geting walkers data");
     return res.sendStatus(500);
   }
 });
