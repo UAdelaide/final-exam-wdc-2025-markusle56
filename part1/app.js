b35c3f630e6493b8fdda46bde293cedf2e4e46ef
ahhkhoa56 <anhkhoa.wm@gmail.com>
2025-06-20 02:20:04 +0000

    [automatic commit] 2025-06-20 02:20:04 UTC 203.221.169.149

--- part1/app.js
+++ part1/app.js
@@ -47,7 +47,7 @@ let db;
 
     // Create a table if it doesn't exist
     await db.execute(`
-      INSERT INTO Users (username, email, password_hash, role) 
+      INSERT INTO Users (username, email, password_hash, role)
       VALUES
       ('alice123', 'alice@example.com', 'hashed123','owner'),
       ('bobwalker', 'bob@example.com', 'hashed456','walker'),
