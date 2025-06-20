1ad938398631e4abb904fe5add3bb93f8dea490f
ahhkhoa56 <anhkhoa.wm@gmail.com>
2025-06-20 10:14:47 +0000

    [automatic commit] 2025-06-20 10:14:47 UTC ;; communications error to 208.67.222.222#53: timed out 203.221.169.149

--- part2/app.js
+++ part2/app.js
@@ -30,7 +30,22 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
-3
+app.get('/api/dogs', async function(req, res) {
+  try {
+    const [rows] = await db.query(
+      `SELECT
+        dog_id,
+        name AS dog_name,
+        size,
+        owner_id
+      FROM Dogs`
+    );
+    return res.status(200).json(rows);
+  } catch(err) {
+    console.error("Error getting dogs data");
+    return res.sendStatus(500);
+  }
+});
 
 // Export the app instead of listening here
 module.exports = app;
