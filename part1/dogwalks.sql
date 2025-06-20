e087501b25395a931e7dadad98925de8522f5f46
ahhkhoa56 <anhkhoa.wm@gmail.com>
2025-06-20 01:51:06 +0000

    [automatic commit] 2025-06-20 01:51:06 UTC 203.221.169.149

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -74,4 +74,4 @@ VALUES
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
 VALUES
-((SELECT dog_id FROM Dogs WHERE name = 'Max'))
+((SELECT dog_id FROM Dogs WHERE name = 'Max' LIMIT 1), '2025-06-10 ')
