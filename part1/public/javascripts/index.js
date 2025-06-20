acda31c0f22c9d860757c8274137956a1fea1234
ahhkhoa56 <anhkhoa.wm@gmail.com>
2025-06-20 03:16:36 +0000

    [automatic commit] 2025-06-20 03:16:36 UTC ;; communications error to 208.67.222.222#53: timed out 203.221.169.149

--- part1/public/javascripts/index.js
+++ part1/public/javascripts/index.js
@@ -7,34 +7,13 @@ createApp({
     };
   },
   async mounted() {
-    // try {
-    //   const res = await fetch('/api/all_book_listing');
-    //   this.book_list = await res.json();
-    // } catch (err) {
-    //   console.error(err);
-    // }
+    try {
+
+    } catch (err) {
+
+    }
   },
   methods: {
-    closeModal() {
-        this.showModal = false;
-        this.book_id = '';
-        this.message = '';
-    },
-    openModal(book) {
-      this.showModal = true;
-      this.selectedBook = book;
-    },
-    sendMessage() {
-      fetch('/message/send', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          book_id: this.selectedBook.book_id,
-          message: this.message
-        })
-      });
-      this.closeModal();
-    }
   }
 })
 .mount('#app');
