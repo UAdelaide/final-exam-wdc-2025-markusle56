8470e0429f8bee85c59f12f3c02bb86968b86ed8
ahhkhoa56 <anhkhoa.wm@gmail.com>
2025-06-20 05:39:31 +0000

    [automatic commit] 2025-06-20 05:39:31 UTC 203.221.169.149

--- part2/app.js
+++ part2/app.js
@@ -19,6 +19,8 @@ app.use(session({
 
 // Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/public')));
 
 // Routes
