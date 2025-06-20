323dcdf9a9607369b5739dc722b036b006b56a92
ahhkhoa56 <anhkhoa.wm@gmail.com>
2025-06-20 05:34:45 +0000

    [automatic commit] 2025-06-20 05:34:45 UTC 203.221.169.149

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -36,9 +36,8 @@ router.get('/me', (req, res) => {
 });
 
 // POST login (dummy version)
-router.post('/login', async function(req, res) {
+router.post('/login', async (req, res) => {
   const { email, password } = req.body;
-
   try {
     const [rows] = await db.query(`
       SELECT user_id, username, role FROM Users
