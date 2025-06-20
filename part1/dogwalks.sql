ce1f5c779d551d672e98005e92a59c35d6346117
ahhkhoa56 <anhkhoa.wm@gmail.com>
2025-06-20 02:12:56 +0000

    [automatic commit] 2025-06-20 02:12:56 UTC 203.221.169.149

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -55,4 +55,38 @@ CREATE TABLE WalkRatings (
 );
 
 
+INSERT INTO Users (username, email, password_hash, role) 
 
+VALUES
+
+('alice123', 'alice@example.com', 'hashed123','owner'),
+
+('bobwalker', 'bob@example.com', 'hashed456','walker'),
+
+('carol123', 'carol@example.com', 'hashed789','owner');
+
+
+
+INSERT INTO Dogs (owner_id, name, size)
+
+VALUES
+
+((SElECT user_id FROM Users WHERE email = 'alice@example.com' LIMIT 1),'Max', 'medium'),
+
+((SElECT user_id FROM Users WHERE email = 'carol@example.com' LIMIT 1), 'Bella', 'small'),
+
+((SElECT user_id FROM Users WHERE email = 'alice@example.com' LIMIT 1), 'Den', 'large'),
+
+((SElECT user_id FROM Users WHERE email = 'carol@example.com' LIMIT 1), 'PhuLoc', 'medium'),
+
+((SElECT user_id FROM Users WHERE email = 'alice@example.com' LIMIT 1), 'LuongPhuoc', 'small');
+
+
+
+INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
+VALUES
+((SELECT dog_id FROM Dogs WHERE name = 'Max' LIMIT 1), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+((SELECT dog_id FROM Dogs WHERE name = 'Bella' LIMIT 1), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
+((SELECT dog_id FROM Dogs WHERE name = 'Bella' LIMIT 1), '2025-07-12 08:00:00', 45, 'Moon', 'cancelled'),
+((SELECT dog_id FROM Dogs WHERE name = 'PhuLoc' LIMIT 1), '2025-04-14 08:00:00', 100, 'Renown Park', 'completed'),
+((SELECT dog_id FROM Dogs WHERE name = 'LuongPhuoc' LIMIT 1), '2025-08-20 08:00:00', 20, 'Fuji', 'open');
