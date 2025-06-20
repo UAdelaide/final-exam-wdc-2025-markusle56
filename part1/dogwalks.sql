490555e3aa113edf6af30bb4faa6e3f1c9dde6d6
ahhkhoa56 <anhkhoa.wm@gmail.com>
2025-06-20 01:56:52 +0000

    [automatic commit] 2025-06-20 01:56:52 UTC 203.221.169.149

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -74,8 +74,8 @@ VALUES
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
 VALUES
-((SELECT dog_id FROM Dogs WHERE name = 'Max' LIMIT 1), '2025-06-10 08:00:00', '30', 'Parklands', 'open'),
+((SELECT dog_id FROM Dogs WHERE name = 'Max' LIMIT 1), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
 ((SELECT dog_id FROM Dogs WHERE name = 'Bella' LIMIT 1), '2025-06-10 09:30:00', '45', 'Beachside Ave', 'accepted'),
-((SELECT dog_id FROM Dogs WHERE name = 'Balle' LIMIT 1), '2025-07-12 08:00:00', '45', 'Moon', 'cancelled'),
+((SELECT dog_id FROM Dogs WHERE name = 'Belle' LIMIT 1), '2025-07-12 08:00:00', '45', 'Moon', 'cancelled'),
 ((SELECT dog_id FROM Dogs WHERE name = 'PhuLoc' LIMIT 1), '2025-04-14 08:00:00', '100', 'Renown Park', 'completed'),
 ((SELECT dog_id FROM Dogs WHERE name = 'LuongPhuoc' LIMIT 1), '2025-08-20 08:00:00', '20', 'Fuji', 'open');
