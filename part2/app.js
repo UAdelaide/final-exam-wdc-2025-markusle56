ecbacd1be6caa26bfb9aaece90a21ffb7c6b2c45
ahhkhoa56 <anhkhoa.wm@gmail.com>
2025-06-20 05:42:35 +0000

    [automatic commit] 2025-06-20 05:42:35 UTC 203.221.169.149

--- part2/app.js
+++ part2/app.js
@@ -6,6 +6,12 @@ var session = require('express-session');
 
 const app = express();
 
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(session({
   secret: 'Exam',
   resave: false ,
@@ -15,13 +21,6 @@ app.use(session({
     maxAge: 1000 * 60 * 60 * 2
   }
 }));
-
-
-// Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '/public')));
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
